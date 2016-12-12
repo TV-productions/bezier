3871e8ea898224942dbb63b0db65e6404bf3dab5
Ties Verschuren <info@tv-productions.org>
2016-12-12 16:49:47 +0100

    Pass BEZIER_BASE_LINK with /

--- bezier_library/src/bezier_library.cpp
+++ bezier_library/src/bezier_library.cpp
@@ -5,9 +5,7 @@ Bezier::Bezier()
   vtk_observer_ = vtkSmartPointer<ErrorObserver>::New();
   ROS_INFO_STREAM("Bezier::Bezier: RViz visualization tool is initialized in 'base' "
                   "and the topic name is 'rviz_visual_tools'");
-  std::string base = BEZIER_BASE_LINK;
-  base.erase(0,1);
-  visual_tools_.reset(new rviz_visual_tools::RvizVisualTools(base));
+  visual_tools_.reset(new rviz_visual_tools::RvizVisualTools(BEZIER_BASE_LINK));
   setDilationParameters(); // Load default dilation parameters
 }
 
